03feb3c7b9cdbe17e5ed01d726cf73fc8d80f5dc
woniu336 <310478073@qq.com>
2024-09-19 16:43:51 +0800

    Update My Site

--- api/upload.js
+++ api/upload.js
@@ -1,16 +1,40 @@
 const fetch = require('node-fetch');
 const FormData = require('form-data');
+const multiparty = require('multiparty');
 
 module.exports = async (req, res) => {
-  if (req.method === 'POST') {
-    const form = new FormData();
-    form.append('file', req.body, {
-      filename: 'image.jpg',
-      contentType: 'image/jpeg',
+  console.log('Received upload request');
+
+  if (req.method !== 'POST') {
+    console.log('Received non-POST request');
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  try {
+    const form = new multiparty.Form();
+
+    const parseForm = () => new Promise((resolve, reject) => {
+      form.parse(req, (err, fields, files) => {
+        if (err) reject(err);
+        else resolve({ fields, files });
+      });
+    });
+
+    const { files } = await parseForm();
+    const file = files.file[0];
+
+    if (!file) {
+      return res.status(400).json({ error: 'No file found in request body' });
+    }
+
+    const formData = new FormData();
+    formData.append('file', file.buffer, {
+      filename: file.originalFilename,
+      contentType: file.headers['content-type'],
     });
 
     const headers = {
-      ...form.getHeaders(),
+      ...formData.getHeaders(),
       'Accept': '*/*',
       'Accept-Encoding': 'gzip, deflate, br',
       'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
@@ -24,28 +48,28 @@ module.exports = async (req, res) => {
       'token': process.env.MEITUAN_TOKEN
     };
 
-    try {
-      const response = await fetch('https://pic-up.meituan.com/extrastorage/new/video?isHttps=true', {
-        method: 'POST',
-        body: form,
-        headers: headers
+    console.log('Sending request to Meituan API');
+    const response = await fetch('https://pic-up.meituan.com/extrastorage/new/video?isHttps=true', {
+      method: 'POST',
+      body: formData,
+      headers: headers,
+    });
+
+    const data = await response.json();
+    console.log('Received response from Meituan API:', data);
+
+    if (data.success === true) {
+      return res.json({
+        Jobs: data.data.originalLink,
+        Name: data.data.originalFileName,
+        os: 'node-oss.zai1.com'
       });
-
-      const data = await response.json();
-
-      if (data.success === true) {
-        res.json({
-          Jobs: data.data.originalLink,
-          Name: data.data.originalFileName,
-          os: 'node-oss.zai1.com'
-        });
-      } else {
-        res.status(400).json({ error: 'Upload failed' });
-      }
-    } catch (error) {
-      res.status(500).json({ error: 'Server error' });
+    } else {
+      console.error('Upload failed:', data);
+      return res.status(400).json({ error: 'Upload failed', details: data });
     }
-  } else {
-    res.status(405).json({ error: 'Method not allowed' });
+  } catch (error) {
+    console.error('Server error:', error);
+    return res.status(500).json({ error: 'Server error', details: error.message });
   }
 };
